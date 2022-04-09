21951d60b3070d21ed34e30f4262a01b4a614dd1
Josefine-Gm-Paaske <74040649+Josefine-Gm-Paaske@users.noreply.github.com>
2022-04-09 21:16:27 +0200

    about created

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,41 @@
 import React from "react";
-
+const styleText = {
+  paddingLeft: "18px",
+  color: "#003690",
+  marginTop: "10px",
+  marginBottom: "0px",
+};
 const About = () => {
-    return (
-        <div
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh'
-            }}
-        >
-            <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, totam.</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 style={styleText}>About me</h1>,
+      <p style={styleText}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Aliquam purus sit
+        amet luctus venenatis lectus magna fringilla urna. Amet venenatis urna
+        cursus eget. Sit amet consectetur adipiscing elit. Tempus urna et
+        pharetra pharetra massa. Tellus rutrum tellus pellentesque eu tincidunt
+        tortor aliquam. Massa vitae tortor condimentum lacinia quis vel eros
+        donec. Ultricies leo integer malesuada nunc vel risus. Enim ut sem
+        viverra aliquet eget sit amet tellus. Facilisi cras fermentum odio eu
+        feugiat pretium. Sit amet commodo nulla facilisi nullam vehicula ipsum a
+        arcu. Non diam phasellus vestibulum lorem. Orci eu lobortis elementum
+        nibh tellus. Enim sit amet venenatis urna cursus. Fusce id velit ut
+        tortor pretium viverra suspendisse. Vel turpis nunc eget lorem dolor.
+        Quisque sagittis purus sit amet volutpat consequat mauris. <p></p> Nisl
+        vel pretium lectus quam id leo in vitae. Proin fermentum leo vel orci
+        porta non pulvinar. Ullamcorper malesuada proin libero nunc consequat.
+        Consectetur adipiscing elit pellentesque habitant. Nibh praesent
+        tristique magna sit. Tincidunt tortor aliquam nulla facilisi cras
+        fermentum odio eu feugiat. Eleifend quam adipiscing vitae proin
+        sagittis. Turpis tincidunt id aliquet risus feugiat in ante metus.
+        Feugiat nisl pretium fusce id velit ut tortor. Volutpat odio facilisis
+        mauris sit amet massa vitae tortor condimentum. Amet justo donec enim
+        diam. In fermentum posuere urna nec tincidunt praesent semper. Id
+        venenatis a condimentum vitae sapien pellentesque.
+      </p>
+    </div>
+  );
 };
 
 export default About;
