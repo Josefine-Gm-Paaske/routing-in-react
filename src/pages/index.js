46b76357430f74cf13bcdbfa9936975c79c5f7cd
Josefine-Gm-Paaske <74040649+Josefine-Gm-Paaske@users.noreply.github.com>
2022-04-10 14:34:30 +0200

    styling set for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+const style = {
+  marginLeft:"20px",
+  marginRight:"20px",
+  color: "rgb(0, 54, 144)"
+};
+
 const Home = () => {
-  return (    
+  return ( 
+    <div style={style}>   
       <h1>Learning React</h1>
+      </div>
   );
 };
 
