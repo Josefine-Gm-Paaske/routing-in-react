ee69acc31a4d82be6079c9e2fdc447e331f63985
Josefine-Gm-Paaske <74040649+Josefine-Gm-Paaske@users.noreply.github.com>
2022-04-10 15:17:51 +0200

    unused import deleted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from './pages/about';
