7540e9c5eed5d889c8e247eeebb497f8737c7bee
Josefine-Gm-Paaske <74040649+Josefine-Gm-Paaske@users.noreply.github.com>
2022-04-10 19:06:12 +0200

    --

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-
-const style = {
-  marginLeft:"20px",
-  marginRight:"20px",
-  color: "rgba(0, 0, 0, 0.65)"
-};
+import React from "react";
 
 const Home = () => {
-  return ( 
-    <div style={style}>   
-      <h1>Learning React</h1>
-      </div>
+  return (
+    <p>test</p>
   );
-};
-
+}
 export default Home;
