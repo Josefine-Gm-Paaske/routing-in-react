9568c1fb32a02ec03c21813aba3f0de93bf97a3f
Josefine-Gm-Paaske <74040649+Josefine-Gm-Paaske@users.noreply.github.com>
2022-04-10 19:20:56 +0200

    Starting the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 
+const img = {
+  width: '98%',
+  height: '150px',
+  objectFit:'cover',
+  marginTop:'10px',
+}
 const Home = () => {
   return (
-    <p>test</p>
+    <div style={{textAlign:'center'}}>
+    <img style={img} src="https://images.unsplash.com/photo-1566296314736-6eaac1ca0cb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80" alt="..."></img>
+    </div>
   );
 }
 export default Home;
