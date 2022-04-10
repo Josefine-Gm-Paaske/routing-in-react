17ea385241d8d37efca3bc93a483c9e19357168c
Josefine-Gm-Paaske <74040649+Josefine-Gm-Paaske@users.noreply.github.com>
2022-04-10 14:32:33 +0200

    sign in and sign up forms - not done

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function SignIn() {
+const style = {
+    backgroundColor: "#2055ae",
+    padding: "100px",
+    width: "220px",
+    margin: "auto",
+    marginTop:"220px",
+    color: "rgb(0, 54, 144)"
+  };
+  
+  const inputFields = {
+    padding: "10px",
+    width: "195px",
+    margin: "auto",
+    color:"rgb(0, 54, 144)"
+  };
+  
+  const FORM_ENDPOINT = ""; // TODO - fill on the later step
+  
+  const SignIn = () => {
+    const [submitted, setSubmitted] = useState(false);
+    const handleSubmit = () => {
+      setTimeout(() => {
+        setSubmitted(true);
+      }, 100);
+    };
+    
+    //User profile
+    if (submitted) {
+      return (
+        <>
+          <div style={{ margin: "20px" , color:"rgb(0, 54, 144)"}} className="text-2xl">
+            Thank you!
+          </div>
+          <div style={{ margin: "20px", color:"rgb(0, 54, 144)"}} className="text-md">
+            We'll be in touch soon.
+          </div>
+        </>
+      );
+    }
+  
     return (
-        <div>
-            <h1>Sign In</h1>
-        </div>
-    )
-}
+      <center>
+        <form
+          style={style}
+          action={FORM_ENDPOINT}
+          onSubmit={handleSubmit}
+          method="POST"
+          target="_blank"
+        >
+          <div style={inputFields} className="mb-3 pt-0">
+            <input
+              type="text"
+              placeholder="Username"
+              name="name"
+              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+              required
+            />
+          </div>
+          <div style={inputFields} className="mb-3 pt-0">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+              required
+            />
+          </div>
+          <div style={{color:"rgb(0, 54, 144)"}} className="mb-3 pt-0">
+            <button className="button" type="submit">
+              Log in
+            </button>
+          </div>
+        </form>
+      </center>
+    );
+  };
+  
 
 export default SignIn;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,11 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function SignUp() {
+const style = {
+    backgroundColor: "#2055ae",
+    padding: "100px",
+    width: "220px",
+    margin: "auto",
+    marginTop:"220px",
+    color: "rgb(0, 54, 144)"
+  };
+  
+  const inputFields = {
+    padding: "10px",
+    width: "195px",
+    margin: "auto",
+    color:"rgb(0, 54, 144)"
+  };
+  
+  const FORM_ENDPOINT = ""; // TODO - fill on the later step
+  
+  const SignUp = () => {
+    const [submitted, setSubmitted] = useState(false);
+    const handleSubmit = () => {
+      setTimeout(() => {
+        setSubmitted(true);
+      }, 100);
+    };
+  
+    //Change this to user profile
+    if (submitted) {
+      return (
+        <>
+          <div style={{ margin: "20px" , color:"rgb(0, 54, 144)"}} className="text-2xl">
+            Thank you!
+          </div>
+          <div style={{ margin: "20px", color:"rgb(0, 54, 144)"}} className="text-md">
+            We'll be in touch soon.
+          </div>
+        </>
+      );
+    }
+  
     return (
-        <div>
-            <h1>Sign Up and get started</h1>
-        </div>
-    )
-}
+      <center>
+        <form
+          style={style}
+          action={FORM_ENDPOINT}
+          onSubmit={handleSubmit}
+          method="POST"
+          target="_blank"
+        >
+          <div style={inputFields} className="mb-3 pt-0">
+            <input
+              type="text"
+              placeholder="Username"
+              name="name"
+              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+              required
+            />
+          </div>
+          <div style={inputFields} className="mb-3 pt-0">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+              required
+            />
+          </div>
+          <div style={{color:"rgb(0, 54, 144)"}} className="mb-3 pt-0">
+            <button className="button" type="submit">
+              Log in
+            </button>
+          </div>
+        </form>
+      </center>
+    );
+  };
+  
+
 
 export default SignUp;
