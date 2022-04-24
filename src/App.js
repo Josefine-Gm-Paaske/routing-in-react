ac3dd69eda62f2347c461a9b506bbdc6d43866a4
Josefine-Gm-Paaske <74040649+Josefine-Gm-Paaske@users.noreply.github.com>
2022-04-24 15:12:46 +0200

    Chat can be hidden or showed on all pages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ import Contact from './pages/contact';
 import Home from './pages';
 import SignIn from './pages/signin';
 import SignUp from './pages/signup';
+import { PubNubProvider } from "pubnub-react";
+import Chat from './components/Chat/chat'
+import PubNub from 'pubnub';
 
+const pn = new PubNub ({
+  publishKey: "pub-c-af9e408a-d4a8-473c-b591-81402cdf9aaf",
+  subscribeKey: "sub-c-7e76d5bc-2658-11e9-9508-c2e2c4d7488a",
+  uuid: 'username',
+});
 
 function App() {
   
@@ -22,6 +30,9 @@ function App() {
         <Route path="/signup" element={<SignUp />}></Route>
       </Routes>
     </BrowserRouter>
+    <PubNubProvider client={pn}>
+      <Chat />
+      </PubNubProvider>
     </div>
   );
 }
